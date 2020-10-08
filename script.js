5e8cb62a62288576873a96aff2998c483f033da5
GBondarek <georgettebondarek@gmail.com>
2020-10-08 10:42:54 -0400

    added id's and <div> began to work on JS to build quiz

---
+++ script.js
@@ -0,0 +1,11 @@
+var quizcontainer = document.getElementById('quiz')
+var resultscontainer = document.getElementById('results')
+var answercontainer = document.getElementById('answer')
+
+Function generate (questions, quizContainer, resultscontainer, answerButton) {
+}
+
+function buildquiz(){}
+function showresults(){}
+
+// start quiz with button
